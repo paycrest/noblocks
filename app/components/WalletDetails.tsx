9c5eac33cb856d80547b25d4526db26fc7073f1f
Jeremy <aworetanjeremiah@gmail.com>
2024-10-23 10:41:32 +0100

    feat: get eth wallet balance for base

--- app/components/WalletDetails.tsx
+++ app/components/WalletDetails.tsx
@@ -1,17 +1,15 @@
 "use client";
 import Image from "next/image";
-import { motion } from "framer-motion";
-import { useRef, useState } from "react";
-import { GoQuestion } from "react-icons/go";
-import { PiCaretDown } from "react-icons/pi";
-
-import { classNames } from "../utils";
+import { useRef, useState, useEffect } from "react";
 import { useOutsideClick } from "../hooks";
-import { FundWalletModal } from "./FundWalletModal";
-import { dropdownVariants } from "./AnimatedComponents";
+import { publicClient } from "../client";
+import { formatEther } from "viem";
+import { usePrivy } from "@privy-io/react-auth";
 
 export const WalletDetails = () => {
+  const { ready, user } = usePrivy();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [balance, setBalance] = useState<string>("");
 
   const dropdownRef = useRef<HTMLDivElement>(null);
   useOutsideClick({
@@ -19,58 +17,48 @@ export const WalletDetails = () => {
     handler: () => setIsOpen(false),
   });
 
+  useEffect(() => {
+    const fetchBalance = async () => {
+      if (!ready || !user?.wallet?.address) return;
+      const balance = await publicClient.getBalance({
+        address: user?.wallet?.address as `0x${string}`,
+      });
+      setBalance(parseFloat(formatEther(balance)).toFixed(4));
+    };
+
+    fetchBalance();
+  }, [ready, user]);
+
+  if (!balance) return null;
+
   return (
     <div ref={dropdownRef} className="relative">
       <button
         type="button"
-        aria-label="Wallet details"
-        aria-expanded={isOpen}
-        aria-haspopup="true"
-        onClick={() => setIsOpen(!isOpen)}
+        title="Wallet balance"
         className="flex items-center justify-center gap-2 rounded-xl bg-gray-50 px-2 shadow focus:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-white active:scale-95 dark:bg-neutral-800 dark:focus-visible:ring-offset-neutral-900"
       >
         <div className="px-0.5 py-2.5">
           <Image
-            src="/logos/coinbase-logo.svg"
-            alt="Coinbase logo"
-            width={20}
-            height={20}
+            src="/logos/privy-logo-black.svg"
+            alt="Privy logo"
+            width={0}
+            height={0}
+            className="size-4 dark:hidden"
+          />
+          <Image
+            src="/logos/privy-logo-white.svg"
+            alt="Privy logo"
+            width={0}
+            height={0}
+            className="hidden size-4 dark:block"
           />
         </div>
         <div className="h-10 w-px border-r border-dashed border-gray-100 dark:border-white/10" />
         <div className="flex items-center gap-2 py-2.5 dark:text-white/80">
-          <p className="hidden sm:block">293 USDC</p>
-          <PiCaretDown
-            className={classNames(
-              "text-base text-gray-400 transition-transform dark:text-white/50",
-              isOpen ? "rotate-180" : "",
-            )}
-          />
+          <p className="pr-1">{balance} ETH</p>
         </div>
       </button>
-
-      {/* Dropdown menu */}
-      {isOpen && (
-        <motion.div
-          initial="closed"
-          animate={isOpen ? "open" : "closed"}
-          exit="closed"
-          variants={dropdownVariants}
-          aria-label="Dropdown menu"
-          className="absolute right-0 z-10 mt-4 max-h-52 min-w-64 max-w-full space-y-4 overflow-y-auto rounded-xl bg-gray-50 p-4 shadow-xl dark:bg-neutral-800"
-        >
-          <p className="text-gray-500 dark:text-white/50">Privy wallet</p>
-          <div className="flex items-center gap-1">
-            <Image
-              src="/logos/usdt-logo.svg"
-              alt="USDC logo"
-              width={14}
-              height={14}
-            />
-            <p>62.8974028 USDT</p>
-          </div>
-        </motion.div>
-      )}
     </div>
   );
 };

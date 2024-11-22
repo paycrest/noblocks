c925480c78dd78c510028d24177d1dd05d397475
Jeremy <aworetanjeremiah@gmail.com>
2024-11-22 22:39:30 +0100

    feat: remove user pill and restore previous navbar components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,13 +2,13 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { usePrivy } from "@privy-io/react-auth";
-import { UserPill } from "@privy-io/react-auth/ui";
+// import { UserPill } from "@privy-io/react-auth/ui";
 
 import { ArrowDownIcon, NoblocksLogo } from "./ImageAssets";
 import { primaryBtnClasses } from "./Styles";
 import { WalletDetails } from "./WalletDetails";
 import { NetworksDropdown } from "./NetworksDropdown";
-// import { SettingsDropdown } from "./SettingsDropdown";
+import { SettingsDropdown } from "./SettingsDropdown";
 import { AnimatedComponent } from "./AnimatedComponents";
 
 export const Navbar = () => {
@@ -54,13 +54,13 @@ export const Navbar = () => {
         <div className="flex gap-4 text-sm font-medium">
           {ready && authenticated ? (
             <>
-              {/* <WalletDetails /> */}
+              <WalletDetails />
 
               <NetworksDropdown />
 
-              {/* <SettingsDropdown /> */}
+              <SettingsDropdown />
 
-              <UserPill size={40} />
+              {/* <UserPill size={36} /> */}
             </>
           ) : (
             <>

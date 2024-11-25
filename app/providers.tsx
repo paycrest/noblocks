480a5dfcfefebe6ebec82df20daba0f5ff22f9d0
Jeremy <aworetanjeremiah@gmail.com>
2024-11-25 23:42:35 +0100

    feat: initialize mixpanel for analytics tracking

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,17 @@
 "use client";
+import mixpanel from "mixpanel-browser";
 import { ThemeProvider } from "next-themes";
 import { ToastContainer } from "react-toastify";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const mixpanelToken = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
+
+  mixpanel.init(mixpanelToken, {
+    debug: true,
+    track_pageview: true,
+    persistence: "localStorage",
+  });
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}

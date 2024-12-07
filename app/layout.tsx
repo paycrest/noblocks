e346a01e29b50b6764056c7155bcff1ffb79b7cb
Jeremy <aworetanjeremiah@gmail.com>
2024-12-07 22:40:24 +0100

    feat: update og image from PNG to GIF format

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     siteName: "Noblocks",
     images: [
       {
-        url: "/images/og-image.png",
+        url: "/images/og-image.gif",
       },
     ],
     locale: "en_US",
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     description:
       "The first interface for decentralized payments to any bank or mobile wallet, powered by a distributed network of liquidity nodes.",
     creator: "@noblocks_xyz",
-    images: ["/images/og-image.png"],
+    images: ["/images/og-image.gif"],
   },
 };
 

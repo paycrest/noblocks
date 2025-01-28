b6cca934ce5a0caae2ffd9c78d605ddace56d503
Jeremy <aworetanjeremiah@gmail.com>
2025-01-28 23:01:33 +0100

    feat(Navbar): add conditional Home link and back button to Privacy and Terms pages

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 import { PrivacyPolicy } from "../components/PrivacyPolicy";
+import { ArrowMoveUpLeftIcon } from "hugeicons-react";
 
 const Privacy = () => {
+  const router = useRouter();
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [showFade, setShowFade] = useState<boolean>(true);
 
@@ -26,14 +29,27 @@ const Privacy = () => {
 
   return (
     <div className="relative h-[80vh]">
-      <div ref={containerRef} className="no-scrollbar h-full overflow-auto">
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="mb-2 flex items-center gap-2 text-sm"
+      >
+        <ArrowMoveUpLeftIcon className="h-4 w-4" />
+        Go back
+      </button>
+
+      <div
+        ref={containerRef}
+        className="no-scrollbar h-full overflow-auto pt-8"
+      >
         <h3 className="text-2xl font-semibold">Privacy Policy</h3>
         <div className="relative">
           <PrivacyPolicy />
         </div>
       </div>
+      
       {showFade && (
-        <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-white to-transparent dark:from-neutral-900" />
+        <div className="pointer-events-none absolute -bottom-9 left-0 right-0 h-16 bg-gradient-to-t from-white via-white to-transparent dark:from-neutral-900 dark:via-neutral-900" />
       )}
     </div>
   );

9ed512b79a70c24864672816a09ec79422acdf7c
Chibuotu Amadi <chibuotu@gmail.com>
2025-02-24 10:53:14 +0100

    fix: optimize order ID reset in transaction flow

--- app/pages/TransactionPreview.tsx
+++ app/pages/TransactionPreview.tsx
@@ -156,9 +156,8 @@ export const TransactionPreview = ({
       });
 
       const params = await prepareCreateOrderParams();
+      setOrderId("");
       setCreatedAt(new Date().toISOString());
-
-      console.log(tokenAddress);
 
       await client?.sendTransaction({
         calls: [
@@ -228,7 +227,6 @@ export const TransactionPreview = ({
   };
 
   const handlePaymentConfirmation = async () => {
-    setOrderId("");
     if (amountSent > (smartWalletBalance?.balances[token] || 0)) {
       toast.warning("Low balance. Fund your wallet.", {
         description: "Insufficient funds. Please add money to continue.",

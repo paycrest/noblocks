b58a9d68a2b5084b7a4c1d883235597980012a39
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-06-12 15:42:53 +0100

    Refactor code structure for improved readability and maintainability

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -306,103 +306,59 @@ export function HomePage({ searchParams }: { searchParams: URLSearchParams }) {
           width={100}
           height={100}
           alt="Walkthrough Video"
-          className="h-auto w-full"
+          className="w-full"
         />
       </div>
 
-      <section className="my-8 flex w-full flex-col items-center justify-center gap-8">
-        <h3 className="text-2xl font-semibold lg:text-3xl">
-          Ways you can use Noblocks
-        </h3>
-        <div className="grid grid-cols-1 gap-4 rounded-[28px] border border-[#FFFFFF1A] p-4 lg:grid-cols-2 lg:gap-8">
-          <div className="flex flex-col gap-6 rounded-[24px] bg-[#202020] px-4 py-8">
-            <h4 className="text-lg font-medium">No Crypto Experience</h4>
-            <p className="flex flex-col gap-4 rounded-[20px] bg-[#FFFFFF0D] p-3">
+      <section className="w-full flex flex-col gap-8 items-center justify-center my-8">
+        <h3 className="font-semibold text-2xl lg:text-3xl">Ways you can use Noblocks</h3>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+          <div className="bg-[#202020] flex flex-col gap-6 rounded-[24px] px-4 py-8">
+            <h4 className="font-medium text-lg">No Crypto Experience</h4>
+            <p className="flex flex-col gap-4 bg-[#FFFFFF0D] rounded-[20px] p-3">
               <span>
                 {/* Icon here */}
-                <Image
-                  src="/images/transfer-stable-coin.svg"
-                  alt="Icon"
-                  width={60}
-                  height={60}
-                />
+                <Image src="/images/transfer-stable-coin.svg" alt="Icon" width={60} height={60}/>
               </span>
-              <span className="text-sm font-normal">
-                Transfer stablecoins to cash in any bank account
-              </span>
+              <span className="font-normal text-sm">Transfer stablecoins to cash in any bank account</span>
             </p>
-            <p className="flex flex-col gap-4 rounded-[20px] bg-[#FFFFFF0D] p-3">
+            <p className="flex flex-col gap-4 bg-[#FFFFFF0D] rounded-[20px] p-3">
               <span>
-                {/* Icon here */}
-                <Image
-                  src="/images/pay-for-groceries.svg"
-                  alt="Icon"
-                  width={60}
-                  height={30}
-                />
+               {/* Icon here */}
+                <Image src="/images/pay-for-groceries.svg" alt="Icon" width={60} height={30}/>
               </span>
-              <span className="text-sm font-normal">
-                Pay for your groceries and expenses swiftly
-              </span>
+              <span className="font-normal text-sm">Pay for your groceries and expenses swiftly</span>
             </p>
-            <p className="flex flex-col gap-4 rounded-[20px] bg-[#FFFFFF0D] p-3">
+             <p className="flex flex-col gap-4 bg-[#FFFFFF0D] rounded-[20px] p-3">
               <span>
                 {/* Icon here */}
-                <Image
-                  src="/images/spend-usdc.svg"
-                  alt="Icon"
-                  width={60}
-                  height={30}
-                />
+                <Image src="/images/spend-usdc.svg" alt="Icon" width={60} height={30}/>
               </span>
-              <span className="text-sm font-normal">
-                Spend USDC/USDT comfortably with no exchange{" "}
-              </span>
+              <span className="font-normal text-sm">Spend USDC/USDT comfortably with no exchange </span>
             </p>
           </div>
-          <div className="flex flex-col gap-6 rounded-[24px] bg-[#202020] px-4 py-8">
-            <h4 className="text-lg font-medium">Web3 Native & Degen</h4>
-            <p className="flex flex-col gap-4 rounded-[20px] bg-[#FFFFFF0D] p-3">
+          <div className="bg-[#202020] flex flex-col gap-6 rounded-[24px] px-4 py-8">
+            <h4 className="font-medium text-lg">Web3 Native & Degen</h4>
+            <p className="flex flex-col gap-4 bg-[#FFFFFF0D] rounded-[20px] p-3">
               <span>
-                {/* Icon here */}
-                <Image
-                  src="/images/turn-defi-tocash.svg"
-                  alt="Icon"
-                  width={60}
-                  height={30}
-                />
+                 {/* Icon here */}
+                <Image src="/images/turn-defi-tocash.svg" alt="Icon" width={60} height={30}/>
               </span>
-              <span className="text-sm font-normal">
-                Turn your DEFI yields into cash easily
-              </span>
+              <span className="font-normal text-sm">Turn your DEFI yields into cash easily</span>
             </p>
-            <p className="flex flex-col gap-4 rounded-[20px] bg-[#FFFFFF0D] p-3">
+            <p className="flex flex-col gap-4 bg-[#FFFFFF0D] rounded-[20px] p-3">
               <span>
                 {/* Icon here */}
-                <Image
-                  src="/images/escape-p2p.svg"
-                  alt="Icon"
-                  width={60}
-                  height={30}
-                />
+                <Image src="/images/escape-p2p.svg" alt="Icon" width={60} height={30}/>
               </span>
-              <span className="text-sm font-normal">
-                Escape P2P and liquidate your cash in no time
-              </span>
+              <span className="font-normal text-sm">Escape P2P and liquidate your cash in no time</span>
             </p>
-            <p className="flex flex-col gap-4 rounded-[20px] bg-[#FFFFFF0D] p-3">
+             <p className="flex flex-col gap-4 bg-[#FFFFFF0D] rounded-[20px] p-3">
               <span>
-                {/* Icon here */}
-                <Image
-                  src="/images/no-issue-dex.svg"
-                  alt="Icon"
-                  width={60}
-                  height={30}
-                />
+                 {/* Icon here */}
+                <Image src="/images/no-issue-dex.svg" alt="Icon" width={60} height={30}/>
               </span>
-              <span className="text-sm font-normal">
-                No issues of losses or security concerns like DEXes{" "}
-              </span>
+              <span className="font-normal text-sm">No issues of losses or security concerns like DEXes </span>
             </p>
           </div>
         </div>

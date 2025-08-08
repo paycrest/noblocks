58f733fd63e2d1d7e615f6682c6d1abcb65b776d
KEM-CONSOLATION <consolationlotachi@gmail.com>
2025-08-08 10:06:29 +0100

    fix farcaster

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 "use client";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { Preloader } from "./components";
 import { MainPageContent } from "./components/MainPageContent";
-import { useEffect } from "react";
 import { useMiniKit } from "@coinbase/onchainkit/minikit";
 
 export default function Page() {
   const { setFrameReady, isFrameReady } = useMiniKit();
 
-  // The setFrameReady() function is called when your mini-app is ready to be shown
   useEffect(() => {
     if (!isFrameReady) {
       setFrameReady();
     }
+
+    if (typeof window !== "undefined" && (window as any).farcaster?.actions) {
+      (window as any).farcaster.actions.ready();
+    }
+
+    console.log("Frame is ready:", isFrameReady);
   }, [setFrameReady, isFrameReady]);
+
   return (
     <Suspense fallback={<Preloader isLoading={true} />}>
       <MainPageContent />

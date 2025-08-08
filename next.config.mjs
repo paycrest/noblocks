75fea884501a13b4d835646323f4ad80747b4223
KEM-CONSOLATION <consolationlotachi@gmail.com>
2025-08-08 10:38:28 +0100

    x frame fix

--- next.config.mjs
+++ next.config.mjs
@@ -16,10 +16,15 @@ const nextConfig = {
           key: "X-Content-Type-Options",
           value: "nosniff",
         },
+        // {
+        //   key: "X-Frame-Options",
+        //   value: "DENY",
+        // },
         {
-          key: "X-Frame-Options",
-          value: "DENY",
+          key: "Content-Security-Policy",
+          value: "frame-ancestors *;",
         },
+
         {
           key: "X-XSS-Protection",
           value: "1; mode=block",

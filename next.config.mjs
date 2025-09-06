05ea5e89d7ad446544ecdf2e22011af630133394
Kem <consolationlotachi@gmail.com>
2025-09-06 11:46:29 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -47,10 +47,6 @@ const nextConfig = {
           key: "Cache-Control",
           value: "no-cache, no-store, must-revalidate",
         },
-        {
-          key: "Content-Security-Policy",
-          value: "default-src 'self'; script-src 'self'",
-        },
       ],
     },
   ],

203188bca637676d11b440c987c4a9904e450efd
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2025-11-06 07:53:04 +0100

    fix(TransactionForm): correct conditional rendering for recipient verification

--- app/pages/TransactionForm.tsx
+++ app/pages/TransactionForm.tsx
@@ -746,9 +746,9 @@ export const TransactionForm = ({
 
         {/* Recipient and memo */}
         <AnimatePresence>
-          {/* {currency &&
+          {currency &&
             (authenticated || isInjectedWallet) &&
-            isUserVerified && ( */}
+            isUserVerified && ( 
               <AnimatedComponent
                 variant={slideInOut}
                 className="space-y-2 rounded-[20px] bg-gray-50 p-2 dark:bg-white/5"
@@ -779,7 +779,7 @@ export const TransactionForm = ({
                   />
                 </div>
               </AnimatedComponent>
-            {/* )} */}
+             )}
         </AnimatePresence>
 
         <AnimatePresence>

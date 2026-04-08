c7b58f149cb6eb1624e703b79dd825cb9094348c
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2026-04-08 17:51:00 +0100

    fix

--- app/components/MainPageContent.tsx
+++ app/components/MainPageContent.tsx
@@ -250,6 +250,7 @@ export function MainPageContent() {
       crossChainBalances,
       injectedAddress,
       injectedReady,
+      embeddedWalletReady,
       isBalanceLoading,
       isInjectedWallet,
       ready,
